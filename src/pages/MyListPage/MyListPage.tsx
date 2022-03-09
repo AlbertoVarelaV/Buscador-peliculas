4b59bb3937149e87f3bb8e5b29152292fed29aef
Alberto Varela <albvar1988@gmail.com>
2022-03-09 17:31:09 +0100

    Show voted movies on MyListPage

--- src/pages/MyListPage/MyListPage.tsx
+++ src/pages/MyListPage/MyListPage.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { RootState } from '../../config/store/store.ts';
+import { startGetVote } from '../../config/actions/myList.ts';
+
+import { useAppDispatch } from '../../hooks/useAppDispatch.ts';
+import { useAppSelector } from '../../hooks/useAppSelector.ts';
+
+import MovieCard from '../../components/MovieCard/MovieCard.tsx';
 
 export function MyListPage() {
+  const dispatch = useAppDispatch();
+
+  const { myList } = useAppSelector((state: RootState) => state.myList);
+
+  useEffect(() => {
+    dispatch(startGetVote());
+  }, [dispatch]);
+
   return (
-    <div>MyListPage</div>
+    <div className="search">
+      <div className="row my-64">
+        {myList
+          && myList.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              id={movie.id}
+              title={movie.title}
+              posterPath={movie.posterPath}
+              releaseDate={movie.releaseDate}
+              overview={movie.overview}
+            />
+          ))}
+      </div>
+    </div>
   );
 }
 
