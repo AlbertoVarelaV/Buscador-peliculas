35c6b3edf0aa979ff17ad06e66b3fc5f408d3940
Alberto Varela <albvar1988@gmail.com>
2022-03-09 16:49:11 +0100

    Styling Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './Header.scss';
+
+import logo from '../../logo.svg';
 
 export function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <nav className="navbar">
+        <div className="container-fluid justify-content-center justify-content-sm-between">
+          <NavLink className="navbar-brand text-white" to="/">
+            <img src={logo} className="logo" alt="logo" />
+          </NavLink>
+          <div>
+            <ul className="nav me-auto my-2 my-lg-0">
+              <li className="nav-item">
+                <NavLink
+                  className={(navlink) => `nav-link${navlink.isActive ? ' active' : ''}`}
+                  to="/"
+                >
+                  Buscador de películas
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 }
 
