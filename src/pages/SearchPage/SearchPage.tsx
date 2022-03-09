1094e142bc822d3704da35acf0faa2bb3c475de1
Alberto Varela <albvar1988@gmail.com>
2022-03-09 17:04:35 +0100

    Get search information and send it to MovieCard

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { RootState } from '../../config/store/store.ts';
+import { startSearchMovies } from '../../config/actions/movies.ts';
+
+import { useAppDispatch } from '../../hooks/useAppDispatch.ts';
+import { useAppSelector } from '../../hooks/useAppSelector.ts';
+import { useForm } from '../../hooks/useForm.ts';
 
 import MovieCard from '../../components/MovieCard/MovieCard.tsx';
 import Pagination from '../../components/Pagination/Pagination.tsx';
 import Search from '../../components/Search/Search.tsx';
 
-import useForm from '../../hooks/useForm.ts';
-
 export function SearchPage() {
+  const dispatch = useAppDispatch();
+
+  const { movies } = useAppSelector((state: RootState) => state.movies);
+
   const [formValue, handleInputChange] = useForm({
     search: '',
   });
+
+  useEffect(() => {
+    dispatch(startSearchMovies(formValue.search, 1));
+  }, [dispatch, formValue]);
 
   return (
     <div className="search">
@@ -18,7 +31,16 @@ export function SearchPage() {
         handleInputChange={handleInputChange}
       />
       <div className="row my-64">
-        <MovieCard />
+        {movies.results
+          && movies.results.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              title={movie.title}
+              posterPath={movie.poster_path}
+              releaseDate={movie.release_date}
+              overview={movie.overview}
+            />
+          ))}
       </div>
       <Pagination />
     </div>
