7dc263731e903de9d1ce880c5e9c1c665a65e767
Alberto Varela <albvar1988@gmail.com>
2022-03-09 16:54:44 +0100

    Define Search Component logical and SearchPage structure

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
-export function Search() {
+type Props = {
+  formValue: {
+    search: string
+  }
+  handleInputChange: () => void
+};
+
+export function Search({ formValue, handleInputChange }: Props) {
   return (
-    <div>Search</div>
+    <div className="form-group">
+      <input
+        autoComplete="off"
+        className="form-control"
+        name="search"
+        onChange={handleInputChange}
+        placeholder="Tu búsqueda"
+        type="search"
+        value={formValue.search}
+      />
+    </div>
   );
 }
 
--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -4,11 +4,22 @@ import MovieCard from '../../components/MovieCard/MovieCard.tsx';
 import Pagination from '../../components/Pagination/Pagination.tsx';
 import Search from '../../components/Search/Search.tsx';
 
+import useForm from '../../hooks/useForm.ts';
+
 export function SearchPage() {
+  const [formValue, handleInputChange] = useForm({
+    search: '',
+  });
+
   return (
-    <div>
-      <Search />
-      <MovieCard />
+    <div className="search">
+      <Search
+        formValue={formValue}
+        handleInputChange={handleInputChange}
+      />
+      <div className="row my-64">
+        <MovieCard />
+      </div>
       <Pagination />
     </div>
   );
